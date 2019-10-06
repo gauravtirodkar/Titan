8eecee3a9b195b658380e4c2c5a281437a43aeb6
Mikespecter <46933684+Mikespecter@users.noreply.github.com>
2019-10-06 23:48:41 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="images/dishes/c.jpg" alt="logo" style="width:60px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" style="margin-left: 500px;font-size: 30px;" href="index.php"><i><strong>GST CAFE</strong></i></a>
+    </li>
+     </ul>
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light "style="background-color: orange">
+  <a class="navbar-brand" href="#" style="margin-left: 30px;">GST CAFE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav" style="margin-left: 600px;">
+      <li class="nav-item active" style="margin-left: 20px;">
+        <a class="nav-link" href="#">MENU<span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item" style="margin-left: 20px;">
+        <a class="nav-link" href="#">TODAY'S SPECIAL</a>
+      </li>
+      <li class="nav-item" style="margin-left: 20px;">
+        <a class="nav-link" href="#about">ABOUT</a>
+      </li>
+      <li class="nav-item" style="margin-left: 20px;">
+        <a class="nav-link" href="#contact">CONTACT</a>
+      </li>
+       <li class="nav-item" style="margin-left: 20px;">
+        <a class="nav-link" href="#faq">FAQs</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+
+    <section style="background-color:  #d6d6c2;">
+<div  class="sec1" id="about">
+<div class="container">
+    <div class="row">
+      <div class="offset-sm-2 col-sm-8">
+        <div class="headerText text-center">    
+          <h2>About Us</h2>
+          <p>
+            What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+<br><br>
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+<br>
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+<br><br>
+Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+ <section style="background-color: #ff8000;">
+<div  class="sec2" id="contact">
+<div class="container">
+    <div class="row">
+      <div class="offset-sm-2 col-sm-8">
+        <div class="headerText text-center">    
+          <h2>Contact</h2>
+          <p>
+           
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section style="background-color: #ffc299">
+<div  class="sec3" id="faq">
+<div class="container">
+    <div class="row">
+      <div class="offset-sm-2 col-sm-8">
+        <div class="headerText text-center">    
+          <h2>FAQs</h2>
+          <p>
+<b>Question 1:</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br>
+<b>Answer 1:</b>
+Lorem Ipsum is simply dummtandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.<br><br>
+<b>Question 2:</b> Lorem Ipsum is simply dummy text of the printing and typesetting .<br>
+<b>Answer 2:</b>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.<br><br>
+<b>Question 3:</b> Lorem Ipsum is simply dummy text of the printing .<br>
+<b>Answer 3:</b>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.<br><br>
+<b>Question 4:</b> Lorem Ipsum is simply and typesetting industry.<br>
+<b>Answer 4:</b>
+Lorem Ipsum is simply dummy text ong and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.<br><br>
+<b>Question 5:</b> Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br>
+<b>Answer 5:</b>
+Lorem Ipsum is simply dummy text orinter took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.<br><br>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
